129c07102a6f38a46e8dbee31b38c34d2f698c91
kamiazya <yuki@kamiazya.tech>
2019-12-16 18:55:51 +0900

    fix internal api, dpt adapter execCommand function return Buffer

--- src/lib/dot-adapter.ts
+++ src/lib/dot-adapter.ts
@@ -1,17 +1,16 @@
 import { execSync } from 'child_process';
 
-function execCommand(command: string, input: string) {
-  const result = execSync(command, {
+function execCommand(command: string, input: string): Buffer {
+  return execSync(command, {
     stdio: 'pipe',
     input,
   });
-  return result.toString();
 }
 
 export function dotStdin(input: string): string {
-  return execCommand('dot -Tdot', input);
+  return execCommand('dot -Tdot', input).toString();
 }
 
 export function jsonStdin(input: string): object {
-  return JSON.parse(execCommand('dot -Tdot_json', input));
+  return JSON.parse(execCommand('dot -Tdot_json', input).toString());
 }
