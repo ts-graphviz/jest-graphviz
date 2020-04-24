7b6250a52bf5d2c8fd5fba7c6433c7c9be7765cc
kamiazya <yuki@kamiazya.tech>
2020-04-24 19:25:23 +0900

    fix build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
 import builtins from 'rollup-plugin-node-builtins';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
 export default {
   input: './src/index.ts',
   output: [
     {
       format: 'cjs',
-      file: './dist/index.js',
+      file: pkg.main,
     },
     {
       format: 'esm',
-      file: './dist/index.mjs',
+      file: pkg.module,
     },
   ],
   plugins: [
